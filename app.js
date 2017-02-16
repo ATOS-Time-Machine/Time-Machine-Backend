672a50aee50d2286a9e02fe66991e5c3fe7a4d9a
giorgio-arena <arena.cpp@gmail.com>
2017-02-16 19:32:59 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env nodejs
 
+const TOKEN_LENGTH = 40;
+const TOKEN_EXPIRY_MINS = 20;
+const TOKEN_CHECK_FREQUENCY_MS = 60 * 60 * 1000;
+const EMAIL_SEND_FREQUENCY_MS = 24 * 60 * 60 * 1000;
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var bcrypt = require('bcrypt-nodejs');
 var randtoken = require('rand-token');
 var date = require('date-and-time');
-
-const TOKEN_LENGTH = 40;
+var excelbuilder = require('msexcel-builder');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 var app = express();
 
@@ -32,16 +38,25 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+//Delete tokens that have been set more than one hour ago
 setInterval(function () {
     console.log("Checking for expired tokens");
-    // Delete tokens that have been set more than one hour ago
+    
     var query = "UPDATE users SET token=NULL, tokenDate=NULL WHERE TIMESTAMPDIFF(minute, tokenDate, NOW()) > ?;";
-    connection.query(query, [20], function (error) {
+    connection.query(query, [TOKEN_EXPIRY_MINS], function (error) {
         if (error) {
             throw error;
         }
     });
-}, (60 * 60 * 1000));  //check every hour
+}, TOKEN_CHECK_FREQUENCY_MS);  //check every hour
+
+//Check every day whether someone should receive monthly excel thinghy
+setInterval(function() {
+	console.log("Checking for monthly whatever");
+
+	workbook = generateExcelFile("tmp.xlsx");
+	sendMail("arena.cpp@gmail.com", "ssup", "<h1> you've been hacked </h1>", "./reports/tmp.xlsx");
+}, EMAIL_SEND_FREQUENCY_MS); //check every day
 
 // Get & post methods go here
 app.post("/register", function (req, res) {
@@ -104,6 +119,48 @@ function updateToken(staffID, token, callback) {
         }
         callback(token);
     });
+}
+
+//Send a mail
+//PS: I had to turn off some security stuff on Gmail
+//PPS: It needs nodejs v6.0 or newer (I needed to update)
+function sendMail(receiver, subject, message, path){
+    var transporter = nodemailer.createTransport(smtpTransport({
+   		service: 'Gmail',
+   		auth: {
+       		user: 'timemachinetest123@gmail.com',
+       		pass: 'giorgiorulez'
+   		}
+	}));
+
+    transporter.sendMail({
+   			from: 'timemachinetest123@gmail.com',
+   			to: receiver,
+   			subject: subject,
+   			html: message,
+   			attachments: [{path: path}]
+	});
+}
+
+//You have to execute sudo node app.js, or it won't have permissions to write this file
+function generateExcelFile(filename)
+{
+ 	var workbook = excelbuilder.createWorkbook('./reports/', filename);
+  
+	var sheet1 = workbook.createSheet('sheet1', 1, 4);
+	  
+	sheet1.set(1, 1, 'You');
+	sheet1.set(1, 2, 'Have');
+	sheet1.set(1, 3, 'Been');
+	sheet1.set(1, 4, 'Hacked');
+	  
+
+	workbook.save(function(ok){
+		if (!ok) 
+		    workbook.cancel();
+		else
+		    console.log('Workbook saved.');
+	});
 }
 
 app.listen(3000, function () {
