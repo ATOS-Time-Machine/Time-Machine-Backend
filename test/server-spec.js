ce9b45b71452dbecde3b3a3260986328cbde5634
TheRurry <zcabrdc@ucl.ac.uk>
2017-03-29 17:10:30 +0100

    Unit testing stuff

--- test/server-spec.js
+++ test/server-spec.js
@@ -1,5 +1,21 @@
 var assert = require('assert');
 
+function auth(connection, body, doneCb) {
+    var query = "SELECT Password FROM Users WHERE StaffID=?;";
+    var parameters = [body.id];
+    connection.query(query, parameters, function (error, results) {
+        if (error) {
+            doneCb(error);
+            return;
+        }
+        if (results[0].Name == 'Jimmy') {
+            doneCb(null, "woo");
+            return;
+        }
+        doneCb("Missing jimmy", null);
+    });
+}
+
 describe('math', function() {
     describe('math works', function() {
         it('adding 1 and 1', function () {
@@ -8,3 +24,46 @@ describe('math', function() {
         });
     });
 });
+
+describe('auth', function() {
+    it('get jimmy', function(done) {
+        var dummyConn = {
+            query: function(q, p, cb) {
+                if (p[0] == 55) {
+                    cb(null, [{Name: "Jimmy"}])
+                } else {
+                    cb("Wrong ID")
+                }
+            }
+        }
+        auth(dummyConn, {id: 55}, function(err, res) {
+            if (err) {
+                done(err);
+                return;
+            }
+            if (res == 'woo') {
+                done();
+            } else {
+                done("Missing woo :(")
+            }
+        });
+    });
+    it('get no jimmy', function(done) {
+        var dummyConn = {
+            query: function(q, p, cb) {
+                if (p[0] == 55) {
+                    cb(null, [{name: "Jimmy"}])
+                } else {
+                    cb("Wrong ID")
+                }
+            }
+        }
+        auth(dummyConn, {id: 56}, function(err, res) {
+            if (err) {
+                done();
+                return;
+            }
+            done("Should fail");
+        });
+    });
+})
