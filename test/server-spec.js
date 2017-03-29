ab794cc6360eb1824f39f6689a25bb31668d55dc
TheRurry <zcabrdc@ucl.ac.uk>
2017-03-29 16:37:45 +0100

    Add test framework

---
+++ test/server-spec.js
@@ -0,0 +1,11 @@
+var server = require("../server");
+var assert = require('assert');
+
+describe('math', function() {
+    describe('math works', function() {
+        it('adding 1 and 1', function () {
+            var result = 1 + 1;
+            assert.equal(result, 2);
+        });
+    });
+});
