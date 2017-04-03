b1825f0235528ea477851ad41c9bf23cfcb85dd8
giorgio-arena <arena.cpp@gmail.com>
2017-04-03 19:08:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -150,14 +150,85 @@ function sendMail(receiver, subject, message){
 function generateExcelFile(filename)
 {
  	var workbook = excelbuilder.createWorkbook('./reports/', filename);
-  
-	var sheet1 = workbook.createSheet('sheet1', 1, 4);
-	  
-	sheet1.set(1, 1, 'You');
-	sheet1.set(1, 2, 'Have');
-	sheet1.set(1, 3, 'Been');
-	sheet1.set(1, 4, 'Hacked');
-	  
+  	var standardFont = 'Arial';
+
+  	var numRows = 40, numCols = 50;
+
+	var sheet1 = workbook.createSheet('sheet1', numCols, numRows);
+
+	for(var i = 0; i < numCols; i++)
+	{
+		sheet1.height(i, 18);
+
+		for(var j = 0; j < numRows; j++)
+		{
+			sheet1.align(i, j, 'center');
+			sheet1.valign(i, j, 'center');
+			sheet1.font(i, j, {name: standardFont, sz:'11'});
+		}
+	}
+	
+	sheet1.width(1, 20);
+	sheet1.height(1, 25);
+	sheet1.font(1, 1, {name: standardFont, sz:'16', iter:'true'});
+	sheet1.set(1, 1, 'Month: ' + date.format(new Date(), 'MMMM  YYYY'));
+
+	sheet1.merge({col:1,row:1},{col:2,row:1});
+
+	sheet1.height(3, 25);
+	sheet1.font(5, 3, {name: standardFont, sz:'22', bold: 'true', iter:'true'});
+	sheet1.set(5, 3, 'SPECIAL PAYMENT CLAIM FORM');
+
+	sheet1.merge({col:5,row:3},{col:9,row:3});
+
+	sheet1.set(1, 6, 'Date');
+
+	sheet1.set(1, 8, 'Overtime');
+	sheet1.set(1, 9, 'Overtime 1.5');
+	sheet1.set(1, 10, 'Overtime 2.0');
+	sheet1.set(1, 11, 'Other hourly rate');
+
+	sheet1.set(1, 12, 'Travel Time');
+	sheet1.set(1, 13, 'Travel Time 0.5BR');
+	sheet1.set(1, 14, 'Travel Time 0.66BR');
+
+	sheet1.font(1, 16, {name: standardFont, sz: '11', bold:'true'});
+	sheet1.set(1, 16, 'On Call (decimals)');//bold
+	sheet1.set(1, 17, 'Primary Hours');
+	sheet1.set(1, 18, 'Secondary Hours');
+	sheet1.set(1, 19, 'Other hourly rate');
+	sheet1.set(1, 20, 'Other On Call amount');
+	sheet1.set(1, 21, '% Salary Enhance (tick)');
+
+	sheet1.set(1, 23, 'Call Out 1.5');
+	sheet1.set(1, 24, 'Call Out 2.0');
+	sheet1.set(1, 25, 'Other rate');
+	sheet1.set(1, 26, 'Other Call Out amount');
+
+	sheet1.height(3, 25);
+	sheet1.font(1, 30, {name: standardFont, sz:'16', bold: 'true'});
+	sheet1.set(1, 30, 'TOTALS FOR PAYMENT');
+
+	sheet1.merge({col:1,row:30},{col:2,row:30});
+
+	var thisMonth = new Date(parseInt(date.format(new Date(), "YYYY")), parseInt(date.format(new Date(), "MM")) - 1, 1);
+	var endOfMonth = date.addDays(date.addMonths(thisMonth, 1), -1);
+
+	var colPos = 3;
+	while(!date.isSameDay(thisMonth, endOfMonth))
+	{
+		sheet1.width(colPos, 15);
+		sheet1.font(colPos, 6, {name: standardFont, sz:'11', iter:'true'});
+		sheet1.set(colPos, 6, date.format(thisMonth, "ddd DD/MM"));
+		colPos++;
+		thisMonth = date.addDays(thisMonth, 1);
+	}
+	sheet1.width(colPos, 15);
+	sheet1.font(colPos, 6, {name: standardFont, sz:'11', iter:'true'});
+	sheet1.set(colPos, 6, date.format(thisMonth, "ddd DD/MM"));
+
+	sheet1.font(colPos + 2, 6, {name: standardFont, sz:'16',bold:'true'});
+	sheet1.set(colPos + 2, 6, 'TOTAL');
 
 	workbook.save(function(ok){
 		if (!ok) 
